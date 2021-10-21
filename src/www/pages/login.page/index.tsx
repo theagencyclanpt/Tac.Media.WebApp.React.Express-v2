c4b812526c45fff16614a705b7cf0d3f231e3174
Luis Rodrigues <luis.ms.rodrigues@hotmail.com>
2021-10-21 12:15:32 +0000

    Logic for test on login page

--- src/www/pages/login.page/index.tsx
+++ src/www/pages/login.page/index.tsx
@@ -1,9 +1,16 @@
+import { useAuthentication } from '@/ui/hooks/authentication.hook';
 import React from 'react';
 
 export function LoginPage(): JSX.Element {
+  const auth = useAuthentication();
+
+  function onLogin() {
+    auth.UserId = 123;
+  }
+
   return (
     <div>
-      <button>Login</button>
+      <button onClick={onLogin}>Login</button>
     </div>
   );
 }
