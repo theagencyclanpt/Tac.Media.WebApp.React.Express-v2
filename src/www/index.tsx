caf6a6290a86f9a1bd642bbc679c3559904221d1
Luis Rodrigues <luis.ms.rodrigues@hotmail.com>
2021-10-21 12:06:19 +0000

    Added auth hook and protected routes

--- src/www/index.tsx
+++ src/www/index.tsx
@@ -1,32 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Pages from "./pages";
 import {
   HashRouter as Router,
-  Route,
 } from "react-router-dom";
+import { AuthenticationProvider } from "@/ui/hooks/authentication.hook";
+import { GenerateRoutes } from "./_routes";
 
 import './index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function GenerateRoutes() {
-  const child: JSX.Element[] = [];
-  Pages.map((page, index) => {
-    child.push(
-      (<Route path={page.route} exact key={index} component={page.component} />)
-    );
-  });
-
-  return (<>
-    {child}
-  </>);
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <GenerateRoutes />
-    </Router>
+    <AuthenticationProvider value={{}}>
+      <Router>
+        <GenerateRoutes RedirectRoute={"/login"} />
+      </Router>
+    </AuthenticationProvider>
   </React.StrictMode>,
   document.getElementById('app')
 );
---
+++ src/www/pages/login.page/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function LoginPage(): JSX.Element {
+  return (
+    <div>
+      <button>Login</button>
+    </div>
+  );
+}
